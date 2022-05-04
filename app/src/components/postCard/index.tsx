05145c7c47a2994c84cef1c09c337bc688ebb92c
anna <anjasudol@gmail.com>
2022-05-04 13:32:14 +0100

    post updated

--- app/src/components/postCard/index.tsx
+++ app/src/components/postCard/index.tsx
@@ -6,8 +6,8 @@ export const PostCard: FC<PostCardData> = ({ title, user }) => {
   return (
     <div className="glass rounded-lg py-4 px-6 bg-white shadow flex flex-col mt-4">
       <h3 className="font-bold text-lg text-gray-600">{title}</h3>
-      <div className="mx-3">
-        <h3 className="font-bold text-gray-600 capitalize">@{truncateAddress(user)}</h3>
+      <div className="my-1">
+        <span className="font-medium text-gray-600">by @{truncateAddress(user)}</span>
       </div>
     </div>
   );
