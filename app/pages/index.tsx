e0376c1e5e0716a28fa18508262cdcb02746f2cc
anna <anjasudol@gmail.com>
2022-05-04 13:14:04 +0100

    added post

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,36 +1,74 @@
 /* eslint-disable no-console */
-import { Button, Footer, Form, Header, NotificationList } from "@components";
+import { Button, ConnectWallet, Footer, Form, Header, NotificationList } from "@components";
 import { appConfig } from "@config";
 import { useInitBlog, useWallet } from "@hooks";
 import { Meta } from "@layout";
 import { notify } from "@utils/notify";
+import { truncateAddress } from "@utils/truncateAddress";
 
 const Home = () => {
-  const { walletAddress } = useWallet();
-  const { isInitBlog, createPost, postList } = useInitBlog(walletAddress);
+  const { walletAddress, connectWallet } = useWallet();
+  const { user, signUpUser, isInitBlog, initBlog, createPost } = useInitBlog(walletAddress);
+
   // const { user, signUpUser, isInitBlog, initBlog } = useBlog(walletAddress);
   const onCreatePost = async (title: string) => {
-    // try {
-    //   const tx = await createPost({ title });
-    //   tx &&
-    //     notify({
-    //       type: "success",
-    //       message: "Signed up user successfully",
-    //       txid: tx as string,
-    //     });
-    // } catch (e) {
-    //   notify({
-    //     type: "error",
-    //     message: "please try later",
-    //   });
-    // }
+    try {
+      const userID = user?.id;
+      const tx = await createPost({ title, userID });
+      tx &&
+        notify({
+          type: "success",
+          message: "Signed up user successfully",
+          txid: tx as string,
+        });
+    } catch (e) {
+      notify({
+        type: "error",
+        message: "please try later",
+      });
+    }
   };
-  console.log(isInitBlog, "")
+  const onSignUpUser = async (name: string) => {
+    try {
+      const tx = await signUpUser({ name });
+      tx &&
+        notify({
+          type: "success",
+          message: "Signed up user successfully",
+          txid: tx as string,
+        });
+    } catch (e) {
+      notify({
+        type: "error",
+        message: "please try later",
+      });
+    }
+  };
 
   return (
     <div className="max-w-7xl mx-auto sm:px-6 flex flex-col items-center justify-center">
       <Meta description={appConfig.description} title={appConfig.title} />
-      <Header />
+      <header className="pt-8 flex flex-col items-center">
+        {!walletAddress ? (
+          <ConnectWallet onClick={connectWallet} />
+        ) : (
+          <>
+            <h3 className="font-bold text-gray-600 capitalize">
+              Connected: @{truncateAddress(walletAddress.toString())}
+            </h3>
+            {user === undefined && (
+              <div className="mt-16">
+                <svg className="animate-spin h-12 w-12 text-white mr-1" viewBox="0 0 24 24">
+                  <path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" fill="#1d4ed8" />
+                </svg>
+              </div>
+            )}
+            {user === null && <Form btnTitle="Sign up" placeholder="User Name" onSubmit={onSignUpUser} />}
+            {user && <p className="font-bold">Logged in as {user?.name}</p>}
+            {isInitBlog && <Button onClick={() => initBlog(walletAddress)}>initBlog</Button>}
+          </>
+        )}
+      </header>
       <main>
         {/* {/* {console.log(isInitBlog, 'isInitBlog')}
         {isInitBlog ? (
@@ -39,7 +77,7 @@ const Home = () => {
           <Form onSubmit={onCreatePost} />
         )} */}
         {/* <Button onClick={() => initBlog(walletAddress)}>initBlog</Button> */}
-        {!!isInitBlog && <Form onSubmit={onCreatePost} />}
+        {isInitBlog === false && <Form onSubmit={onCreatePost} />}
       </main>
       <Footer />
       <NotificationList />
--- app/src/components/postCard/index.tsx
+++ app/src/components/postCard/index.tsx
@@ -1,7 +1,8 @@
+import { PostCardData } from "@types";
 import { truncateAddress } from "@utils";
 import { FC } from "react";
 
-export const PostCard: FC<PostCard> = ({ title, user }) => {
+export const PostCard: FC<PostCardData> = ({ title, user }) => {
   return (
     <div className="glass rounded-lg py-4 px-6 bg-white shadow flex flex-col mt-4">
       <h3 className="font-bold text-lg text-gray-600">{title}</h3>
