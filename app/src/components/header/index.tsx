ac9ffcf44e4080c2ed1bbcccdcba5aa1d10546ba
anna <anjasudol@gmail.com>
2022-05-04 09:56:53 +0100

    added header

--- app/src/components/header/index.tsx
+++ app/src/components/header/index.tsx
@@ -28,17 +28,21 @@ export const Header = () => {
       {!walletAddress ? (
         <ConnectWallet onClick={connectWallet} />
       ) : (
-        <h3 className="font-bold text-gray-600 capitalize">Connected: @{truncateAddress(walletAddress.toString())}</h3>
+        <>
+          <h3 className="font-bold text-gray-600 capitalize">
+            Connected: @{truncateAddress(walletAddress.toString())}
+          </h3>
+          {user === undefined && (
+            <div className="mt-16">
+              <svg className="animate-spin h-12 w-12 text-white mr-1" viewBox="0 0 24 24">
+                <path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" fill="#1d4ed8" />
+              </svg>
+            </div>
+          )}
+          {user === null && <Form btnTitle="Sign up" placeholder="User Name" onSubmit={onSignUpUser} />}
+          {user && <p className="font-bold">Logged in as {user?.name}</p>}
+        </>
       )}
-      {user === undefined && (
-        <div className="mt-12">
-          <svg className="animate-spin h-12 w-12 text-white mr-1" viewBox="0 0 24 24">
-            <path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" fill="#1d4ed8" />
-          </svg>
-        </div>
-      )}
-      {user === null && <Form btnTitle="Sign up" placeholder="User Name" onSubmit={onSignUpUser} />}
-      {user && <p className="font-bold">Logged in as {user?.name}</p>}
     </header>
   );
 };
