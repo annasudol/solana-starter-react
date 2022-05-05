21419e09e84165e70d6c86472508dee292e8e3f6
anna <anjasudol@gmail.com>
2022-05-05 13:07:37 +0100

    keygen updates

--- app/scripts/createKeyPair.ts
+++ app/scripts/createKeyPair.ts
@@ -3,8 +3,6 @@ import * as fs from "fs";
 
 const initBlogAccount = anchor.web3.Keypair.generate();
 const genesisPostAccount = anchor.web3.Keypair.generate();
-const postAccount = anchor.web3.Keypair.generate();
 
 fs.writeFileSync("./keys/initBlogAccount-keypair.json", JSON.stringify(initBlogAccount));
 fs.writeFileSync("./keys/genesisPostAccount-keypair.json", JSON.stringify(genesisPostAccount));
-fs.writeFileSync("./keys/postAccount-keypair.json", JSON.stringify(postAccount));
